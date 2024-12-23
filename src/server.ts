5b161363107310caeb41b525aeabd504931d4a35
md monjur bakth mazumder <md.monjurmbm2001@gmail.com>
2024-12-23 20:53:24 +0600

    update

--- src/server.ts
+++ src/server.ts
@@ -6,12 +6,11 @@ import mongoose from 'mongoose';
 async function main() {
   try {
     await mongoose.connect(config.database_url as string);
-
     app.listen(config.port, () => {
-      console.log(`Example app listening on port ${config.port}`);
+      console.log(`App listening on port ${config.port}`);
     });
-  } catch (error) {
-    console.log(error);
+  } catch (err) {
+    console.log(err);
   }
 }
 main();
